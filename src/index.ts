29d641f62d196f24d1ea67ca46d4dc11fc05e536
Jordan Hansen <jhansen1@lenovo.com>
2019-11-27 13:36:33 -0700

    full puppeteer working

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,76 @@
 import * as json2csv from 'json2csv';
 import * as fs from 'fs';
 import puppeteer from 'puppeteer';
+import cheerio from 'cheerio';
 
 
 (async () => {
-	const browser = await puppeteer.launch({ headless: false });
-	// const page = await browser.newPage();
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-	// await page.goto('https://pizza.com');
+    await page.goto('http://pizza.com');
 
-	// // Search by element
-	// const title = await page.$eval('title', element => element.text);
+    // Search by element
+    const title = await page.$eval('title', element => element.textContent);
 
-	// console.log('title', title);
+    console.log('title', title);
 
-	// await browser.close();
+    // Search by class
+    const homeButton = await page.$eval('.home_link', element => element.textContent);;
 
-	// Search by class
-	// const homeButton = $('.home_link').text();
+    console.log('Home button', homeButton);
 
-	// console.log('Home button', homeButton);
+    // Search by class and child
+    const topNavButtons = await page.$eval('.word-only li', element => element.textContent);
 
-	// // Search by class and child
-	// const topNavButtons = $('.word-only li').text();
+    console.log('top nav buttons', topNavButtons);
 
-	// console.log('top nav buttons', topNavButtons);
+    // // Search by property
+    const pizzaNews = await page.$eval('a[href="/pizza-news"]', element => element.textContent);
 
-	// // Search by property
-	// const pizzaNews = $('a[href="/pizza-news"]').text();
+    console.log('pizza news', pizzaNews);
 
-	// console.log('pizza news', pizzaNews);
+    // // Search by property and find only the last
+    const lastNavLink = await page.$$eval('li a', elements => elements[elements.length - 1].textContent);
 
-	// // Search by property and find only the first
-	// const firstNavLink = $('li a').first().text();
+    console.log('last  nav link', lastNavLink);
 
-	// console.log('first nav link', firstNavLink);
+    // // Get propery from element
+    const funFactsLink = await page.$eval('.last a', element => element.getAttribute('href'));
 
-	// // Search by property and find only the last
-	// const lastNavLink = $('li a').last().text();
+    console.log('fun facts link', funFactsLink);
 
-	// console.log('last  nav link', lastNavLink);
+    // Get a list of all 'li a' text
+    const listElements: any[] = await page.evaluate(() => Array.from(document.querySelectorAll('li a'), element => element.textContent));
 
-	// // Get propery from element
-	// const funFactsLink = $('.last a').prop('href');
+    console.log('list elements', listElements);
 
-	// console.log('fun facts link', funFactsLink);
+    const data = {
+        titleText: title,
+        homeButtonText: homeButton,
+        topNavButtonsText: topNavButtons,
+        pizzaNewsText: pizzaNews,
+        listElementsArray: listElements
+    };
 
-	// const listElements: any[] = [];
+    const csv = json2csv.parse(data);
 
-	// // Access each of a list in a loop
-	// $('li').each(function (index, element) {
-	// 	const elementText = $(element).text();
-	// 	listElements.push(elementText);
-	// 	console.log('this text', $(element).text());
-	// });
+    await page.click('a[href="/pizza-news"]');
+    // Wait for 3.5 seconds
+    await page.waitFor(3500);
 
-	// const data = {
-	// 	titleText: title,
-	// 	homeButtonText: homeButton,
-	// 	topNavButtonsText: topNavButtons,
-	// 	pizzaNewsText: pizzaNews,
-	// 	listElementsArray: listElements
-	// };
+    // ...or just use cheerio
+    const bodyHtml = await page.evaluate(() => document.body.innerHTML);
+    const $ = cheerio.load(bodyHtml);
+    console.log('pizza news type-post', $('.type-post b').text());
 
-	// const csv = json2csv.parse(data);
+    fs.writeFile('data.csv', csv, (err) => {
+        if (err) {
+            return console.log('an error happened while saving the file', err);
+        }
+        console.log('file saved successfully!');
+    });
 
-	// fs.writeFile('data.csv', csv, (err) => {
-	// 	if (err) {
-	// 		return console.log('an error happened while saving the file', err);
-	// 	}
-	// 	console.log('file saved successfully!');
-	// });
 
+    await browser.close();
 })();
