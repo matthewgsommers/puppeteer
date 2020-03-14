8fe583cd6287428497cb485000c92ff70c76c067
Matthew Sommers <mgsommers@mintz.com>
2020-03-14 13:30:45 -0400

    1st commit. Create app for scraping news articles. the economist and the verge are the 1st subject. Currently works for a singular URL, scrapes site, gets article title and body. Checks for file directory strucure, creates it if it does not exist, saves article. Can send email info via gmail.

--- src/index.ts
+++ src/index.ts
@@ -2,89 +2,122 @@ import * as json2csv from 'json2csv';
 import * as fs from 'fs';
 import puppeteer from 'puppeteer';
 import cheerio from 'cheerio';
+import gmailSend from 'gmail-send';
 
-(async () => {
+// Typescript
+// app.tsimport data from './example.json';
+// const word = (<any>data).name;
+// console.log(word); // output 'testing'
 
+let storageDirectoryName = 'storageDirectory';
 
-    let browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--incognito',
-        ],
+function createStorageDirectory(storageDirectory, directoryName) {
+    if (!fs.existsSync('./' + directoryName)){
+        fs.mkdirSync('./' + storageDirectory);
+        console.log('/' + storageDirectory + ' Has Been Created');
+    } else {
+        console.log( storageDirectory + ' Directory Exists Already');
+    }
+}
+
+function createPublisherDirectory(storageDirectory, directoryName) {
+    if (!fs.existsSync('./' + storageDirectory +'/' + directoryName)){
+        fs.mkdirSync('./' + storageDirectory + '/' + directoryName);
+        console.log('./' + storageDirectory + directoryName + ' Has Been Created');
+    } else {
+        console.log('./' + storageDirectory + directoryName + ' Directory Exists Already');
+    }
+}
+
+function getSiteName(url) {
+    let endOfWww = url.indexOf('www') + 4;
+    // @TODO: Account for non .com URLs
+    let startOfDotCom = url.indexOf('.com');
+    return url.substring(endOfWww, startOfDotCom);
+}
+
+function formatString(string) {
+    let formattedString = string.toLowerCase().replace(/ /g,"-");
+    return formattedString;
+}
+
+function sendEmail() {
+    const send = require('gmail-send')({
+        user: '',
+        pass: '',
+        to:   '',
+        subject: 'this is a test',
+        body: 'emailBody',
     });
+    send({
+        text:    'gmail-send example 1',
+        },
+        (error, result, fullResult) => {
+            if (error) console.error(error);
+            console.log(result);
+        })
+}
 
-    let getName = function(urlArray) {
-        for (let i = 0; i < urlArray.length; i++) {
-        console.log("Targeting" + urlArray[0]);
-        let endOfWww = urlArray[i].indexOf('www') + 4;
-        let startOfDotCom = urlArray[i].indexOf('.com');
-        let siteName = urlArray[i].substring(endOfWww, startOfDotCom);
-        console.log("Site name is: " + siteName);
-    }
+function launchPuppetter() {
+   try {
+        (async () => {
 
+            let browser = await puppeteer.launch({
+                headless: true,
+                args: [
+                    '--incognito',
+                ],
+            });
 
+            let url = [
+                'https://www.economist.com/leaders/2020/02/20/how-to-make-sense-of-the-latest-tech-surge'
+            ];
 
+            for (let i = 0; i < url.length; i++) {
+                const context = await browser.createIncognitoBrowserContext();
+                const page = await context.newPage();
 
-    let url = [
-        'https://www.economist.com/leaders/2020/02/20/how-to-make-sense-of-the-latest-tech-surge'
-    ];
+                await page.goto(url[i], { waitUntil: 'domcontentloaded' });
+                await page.evaluate( () => { debugger; } );
 
-    for (let i = 0; i < url.length; i++) {
-        const context = await browser.createIncognitoBrowserContext();
-        // implement try/catch pattern
-        // Get publisher's domain name
+                // Get Article title
+                let rawTitle = await page.$eval('title', element => element.textContent);
+                let title = formatString(rawTitle);
 
+                // Get body HTML
+                let bodyHtml = await page.evaluate(() => document.body.innerHTML);
 
-        // Init. Puppeteer
-        let page = await context.newPage();
-        await page.goto(url[i], { waitUntil: 'domcontentloaded' });
-        await page.evaluate( () => { debugger; } );
+                // Init cheerio
+                let $ = cheerio.load(bodyHtml);
 
-        // Get Article title
-        let rawTitle = await page.$eval('title', element => element.textContent);
-        let title;
-        if (rawTitle) {
-            title = rawTitle.toLowerCase().replace(/ /g,"_");
-        }
+                let body = $('.article__body-text').text().replace(/^\s*[\r\n]/gm, "\r\n");
+                // /(.{80})/g
 
-        // Get body HTML
-        let bodyHtml = await page.evaluate(() => document.body.innerHTML);
+                let siteName = getSiteName(url[i]);
+                createPublisherDirectory(storageDirectoryName, siteName);
 
-        // Init cheerio
-        let $ = cheerio.load(bodyHtml);
+                // Concat all relevant data points into one variable
+                let data = siteName + "\r\n" + "\r\n" + title + "\r\n" + body;
 
-        // Target content container class
-        let rawArticleBody = $('.blog-post__text p').text();
-
-        // let formattedArticleBody = rawArticleBody.replace(/^\s*[\r\n]/gm, "\r\n");
-        let formattedArticleBody = rawArticleBody.replace(/^\s*[\r\n]/gm, "\r\n");
-        console.log(formattedArticleBody);
-        // /(.{80})/g
-
-
-        // Concat all relevant data points into one variable
-        let data = siteName + "\r\n" + "\r\n" + title + "\r\n" + formattedArticleBody;
-
-        if (!fs.existsSync('./scrapedArticles')){
-            fs.mkdirSync('./scrapedArticles');
-        } else {
-            console.log('Scraped Articles Directory Exists Already');
-        }
-
-        // Check if a directory for the scrapped site already exists. If not, create it.
-        if (!fs.existsSync('./scrapedArticles/' + siteName)){
-            fs.mkdirSync('./scrapedArticles/' + siteName);
-        } else {
-            console.log(siteName + ' Directory Exists Already');
-        }
-
-        // Create file with title as name. Insert website name, article title, and article body
-        fs.writeFile("./scrapedArticles/" + siteName + "/" + title + '.txt', data, (err) => {
-            if (err) {
-                return console.log('an error happened while saving the file', err);
+                // Create file with title as name. Insert website name, article title, and article body
+                fs.writeFile("./" + storageDirectoryName + "/" + siteName + "/" + title + '.txt', data, (err) => {
+                    if (err) {
+                        return console.log('an error happened while saving the file', err);
+                    }
+                    console.log('file saved successfully!' + "\r\n");
+                });
             }
-            console.log('file saved successfully!' + "\r\n");
-        });
-    }
-    await browser.close();
-})();
+            await browser.close();
+        })()
+   } catch {
+       console.log('Puppeteer has failed. See logs for errors.');
+   }
+};
+
+function initiateScrape() {
+    createStorageDirectory(storageDirectoryName, 'storageDirectory' );
+    launchPuppetter();
+    sendEmail();
+}
+
+initiateScrape();
