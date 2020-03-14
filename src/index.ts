2102e6085654c0e405f297d19ca6b6f3a86114a3
Matthew Sommers <mgsommers@mintz.com>
2020-03-14 14:52:18 -0400

    Make more improvements. Read all files from a directory, abstract the process a bit more.

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,55 @@ import * as fs from 'fs';
 import puppeteer from 'puppeteer';
 import cheerio from 'cheerio';
 import gmailSend from 'gmail-send';
-
+import path from 'path';
 // Typescript
 // app.tsimport data from './example.json';
 // const word = (<any>data).name;
 // console.log(word); // output 'testing'
 
+// @TODO: Change flow of app. User inputs the site URL, go to homepage, create array of all <a> tags, visit all tags in
+// the array, collect all of the articles.
+
 let storageDirectoryName = 'storageDirectory';
+let filePathArray : string[] = [];
+
+function getAllFiles() {
+    fs.readdir('storageDirectory/economist', function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        files.forEach(function (file) {
+            console.log(file);
+            filePathArray.push(file);
+        });
+    });
+    console.log(filePathArray);
+}
 
 function createStorageDirectory(storageDirectory, directoryName) {
-    if (!fs.existsSync('./' + directoryName)){
-        fs.mkdirSync('./' + storageDirectory);
-        console.log('/' + storageDirectory + ' Has Been Created');
+    let filePath = './' + directoryName;
+    if (!fs.existsSync(filePath)){filePathArray
+        fs.mkdirSync(filePath);
     } else {
-        console.log( storageDirectory + ' Directory Exists Already');
+        // Do nothing
     }
 }
 
 function createPublisherDirectory(storageDirectory, directoryName) {
-    if (!fs.existsSync('./' + storageDirectory +'/' + directoryName)){
-        fs.mkdirSync('./' + storageDirectory + '/' + directoryName);
-        console.log('./' + storageDirectory + directoryName + ' Has Been Created');
+    let filePath = './' + storageDirectory + '/' + directoryName;
+    if (!fs.existsSync(filePath)){
+        fs.mkdirSync(filePath);
     } else {
-        console.log('./' + storageDirectory + directoryName + ' Directory Exists Already');
+        // Do nothing
     }
 }
 
 function getSiteName(url) {
-    let endOfWww = url.indexOf('www') + 4;
     // @TODO: Account for non .com URLs
+    let endOfWww = url.indexOf('www') + 4;
     let startOfDotCom = url.indexOf('.com');
-    return url.substring(endOfWww, startOfDotCom);
+    let siteName = url.substring(endOfWww, startOfDotCom);
+    return siteName
 }
 
 function formatString(string) {
@@ -42,15 +60,25 @@ function formatString(string) {
 }
 
 function sendEmail() {
+    console.log('comig from send email');
+    console.log(filePathArray);
+    let fileArray =  [
+        'storageDirectory/economist/big-tech---how-to-make-sense-of-the-latest-tech-surge-|-leaders-|-the-economist.txt',
+        'storageDirectory/economist/test.txt'
+    ]
     const send = require('gmail-send')({
         user: '',
         pass: '',
         to:   '',
         subject: 'this is a test',
         body: 'emailBody',
+        files: [ filePathArray ]
     });
+
+    // files: [ filePathArray ],
+    // @TODO: Ge the path of all files that have been created.
     send({
-        text:    'gmail-send example 1',
+        text: 'gmail-send example 1',
         },
         (error, result, fullResult) => {
             if (error) console.error(error);
@@ -70,7 +98,8 @@ function launchPuppetter() {
             });
 
             let url = [
-                'https://www.economist.com/leaders/2020/02/20/how-to-make-sense-of-the-latest-tech-surge'
+                'https://www.economist.com/leaders/2020/02/20/how-to-make-sense-of-the-latest-tech-surge',
+                'https://www.economist.com/briefing/2020/03/12/understanding-sars-cov-2-and-the-drugs-that-might-lessen-its-power'
             ];
 
             for (let i = 0; i < url.length; i++) {
@@ -90,21 +119,26 @@ function launchPuppetter() {
                 // Init cheerio
                 let $ = cheerio.load(bodyHtml);
 
+
+                // @TODO: Use data object to set the regex dynamically depending on what url is passed in
+                // @TODO: Figure out a way to format text so that its broken into paragraphs
                 let body = $('.article__body-text').text().replace(/^\s*[\r\n]/gm, "\r\n");
                 // /(.{80})/g
-
                 let siteName = getSiteName(url[i]);
-                createPublisherDirectory(storageDirectoryName, siteName);
+                await createPublisherDirectory(storageDirectoryName, siteName);
 
                 // Concat all relevant data points into one variable
                 let data = siteName + "\r\n" + "\r\n" + title + "\r\n" + body;
+                let filePath = "./" + storageDirectoryName + "/" + siteName + "/" + title + '.txt';
 
+                // @TODO: Replace with a function to create the paths instead of doing it here
+                filePathArray.push(filePath);
                 // Create file with title as name. Insert website name, article title, and article body
-                fs.writeFile("./" + storageDirectoryName + "/" + siteName + "/" + title + '.txt', data, (err) => {
+                fs.writeFile(filePath, data, (err) => {
                     if (err) {
                         return console.log('an error happened while saving the file', err);
                     }
-                    console.log('file saved successfully!' + "\r\n");
+                    // Do nothing
                 });
             }
             await browser.close();
@@ -114,10 +148,11 @@ function launchPuppetter() {
    }
 };
 
-function initiateScrape() {
-    createStorageDirectory(storageDirectoryName, 'storageDirectory' );
-    launchPuppetter();
-    sendEmail();
+async function initiateScrape() {
+    await createStorageDirectory(storageDirectoryName, 'storageDirectory' );
+    await launchPuppetter();
+    await getAllFiles();
+    await sendEmail();
 }
 
 initiateScrape();
